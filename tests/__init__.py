33271a22404add4c37e5b13e50a51b48725bc023
tristanlatr <tris.la.tr@gmail.com>
2022-09-01 13:48:13 -0400

    Try ignoring mod1 as well.

--- tests/__init__.py
+++ tests/__init__.py
@@ -20,10 +20,7 @@ def parse_randommodules(nodes:rewriter.NodesT=nodes,
                             rewritercls:Type[rewriter.Rewriter]=rewriter.Rewriter) -> Iterator[Tuple[ast.Module, nodes.Module]]:
     for entry in (Path(__file__).parent / 'randommodules').iterdir():
         if entry.is_file():
-            # Don't parse mod4 if we're on python 3.6, it triggers the following error:
-            # E           async with haploperistomic as epidiascopic, {(misrehearse is not judgeship):
-            # E                    ^
-            # E       SyntaxError: invalid syntax
-            if entry.stem=='mod4' and sys.version_info < (3,7):
+            # Don't parse some modules if we're on python 3.6, it triggers invalid syntax errors.
+            if entry.stem in ['mod4', 'mod1'] and sys.version_info < (3,7):
                 continue
             yield parse_mod('randommodules', entry.stem, nodes=nodes, rewritercls=rewritercls)
